9b4591b1b27af95cab9a444be396a49963e1b461
Akash0110-hub <akashgshettar10@gmail.com>
2025-12-02 12:19:44 +0530

    Update student.py

--- student.py
+++ student.py
@@ -5,11 +5,12 @@ def student_registration(student_id, student_name, course_enrolled, academic_yea
         f"Course Enrolled  : {course_enrolled}\n"
         f"Academic Year    : {academic_year}"
     )
-if __name__ == "__main__":
-    #sample output
-    student_id:"0110"
-    student_name:"Aakash"
-    course_enrolled: "Bca"
-    academic_year:"2025-26"
-    print(student_registration(student_id, student_name, course_enrolled, academic_year))
 
+if __name__ == "__main__":
+    # sample output values
+    student_id = "0110"
+    student_name = "Aakash"
+    course_enrolled = "BCA"
+    academic_year = "2025-26"
+
+    print(student_registration(student_id, student_name, course_enrolled, academic_year))
